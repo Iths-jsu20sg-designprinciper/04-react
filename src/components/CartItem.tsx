06170600a7201644b4dc018990975e16b1704a57
David Andersson <david.andersson@zocom.se>
2021-11-30 10:57:17 +0100

    Test case: add

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,14 +1,19 @@
+import { useState } from 'react'
 import './CartItem.css'
 
-const CartItem = () => (
-	<div className="cart-item">
-		<h3>Snöskyffel</h3>
-		<span> 60 kr </span>
-		<span className="amount">1</span>
-		<button> Lägg till </button>
-		<button> Ta bort </button>
-	</div>
-)
+const CartItem = () => {
+	const [amount, setAmount] = useState(1)
+
+	return (
+		<div className="cart-item">
+			<h3>Snöskyffel</h3>
+			<span> 60 kr </span>
+			<span className="amount">{amount}</span>
+			<button onClick={() => setAmount(amount + 1)}> Lägg till </button>
+			<button> Ta bort </button>
+		</div>
+	)
+}
 // .cart-item
 // .cart-item h3
 // .cart-item button
